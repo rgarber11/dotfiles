ac35764a83a773a81f222ea4d5c92a80aa970702
Richard Garber <richard.garber@ufl.edu>
2024-05-28 18:02:05 +0800

    Added Latex Toggle to Neorg, as Nvim v0.10 is now stable

--- nvim/lua/norg-latex.lua
+++ nvim/lua/norg-latex.lua
@@ -1,14 +1,14 @@
 -- Toggle Latex Rendering
- local latex_enabled_buffers = {}
- function toggle_latex_rendering()
-   local buffer = vim.api.nvim_get_current_buf()
-   if latex_enabled_buffers[buffer] then
-     latex_enabled_buffers[buffer] = false
-     vim.cmd('Neorg clear-latex')
-   else
-     latex_enabled_buffers[buffer] = true
-     vim.cmd('Neorg render-latex')
-   end
- end
+local latex_enabled_buffers = {}
+function toggle_latex_rendering()
+    local buffer = vim.api.nvim_get_current_buf()
+    if latex_enabled_buffers[buffer] then
+        latex_enabled_buffers[buffer] = false
+        vim.cmd "Neorg clear-latex"
+    else
+        latex_enabled_buffers[buffer] = true
+        vim.cmd "Neorg render-latex"
+    end
+end
 
- vim.keymap.set('n', '<leader>mt', toggle_latex_rendering, { desc = 'Toggle Neorg Latex Rendering' })
+vim.keymap.set("n", "<leader>mt", toggle_latex_rendering, { desc = "Toggle Neorg Latex Rendering" })
