bfec90eebc0e984050a4a0c413a006c9aa611cb5
Richard Garber <rg.1029384756@gmail.com>
2024-08-27 14:58:40 -0400

    Added Mini.nvim, removed LuaLine and Comment.nvim for Mini alternatives

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,54 +1,9 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 -- Example for configuring Neovim to load user-installed installed Lua rocks:
 package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -61,24 +16,10 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require("lazy").setup({
-    -- NOTE: First, some plugins that don't require any configuration
-
-    -- Git related plugins
     "tpope/vim-fugitive",
     "tpope/vim-rhubarb",
-
-    -- Detect tabstop and shiftwidth automatically
     "tpope/vim-sleuth",
-
-    -- NOTE: This is where your plugins related to LSP can be installed.
-    --  The configuration is done below. Search for lspconfig to find it below.
     {
         -- LSP Configuration & Plugins
         "neovim/nvim-lspconfig",
@@ -179,21 +120,6 @@ require("lazy").setup({
             vim.cmd.colorscheme "solarized-osaka"
         end,
     },
-
-    {
-        -- Set lualine as statusline
-        "nvim-lualine/lualine.nvim",
-        -- See `:help lualine.txt`
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = "solarized-osaka",
-                component_separators = "|",
-                section_separators = "",
-            },
-        },
-    },
-
     {
         -- Add indentation guides even on blank lines
         "lukas-reineke/indent-blankline.nvim",
@@ -203,8 +129,6 @@ require("lazy").setup({
         opts = {},
     },
 
-    -- "gc" to comment visual regions/lines
-    { "numToStr/Comment.nvim", opts = {} },
     {
         "nvimdev/hlsearch.nvim",
         event = "BufRead",
@@ -537,7 +461,8 @@ cmp.setup({
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,15 @@
+return {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+        require("mini.ai").setup({ n_lines = 500 })
+        require("mini.surround").setup()
+        require("mini.comment").setup()
+        require("mini.pairs").setup()
+        require("mini.git").setup()
+        require("mini.diff").setup()
+        local statusline = require "mini.statusline"
+        -- set use_icons to true if you have a Nerd Font
+        statusline.setup({ use_icons = vim.g.have_nerd_font })
+    end,
+}
