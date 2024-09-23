91c5974f8504db8b5d5647d2223c2ead6bd4f540
Richard Garber <rg.1029384756@gmail.com>
2024-09-23 17:23:10 -0400

    Made hyprctl wrapper more Pythonic

--- nwg-panel/executors/keyboard_layout.py
+++ nwg-panel/executors/keyboard_layout.py
@@ -12,63 +12,61 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def hyprctl(cmd, buf_size=2048):
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    socket_path = Path(
-        os.getenv("XDG_RUNTIME_DIR"),
-        "hypr",
-        os.getenv("HYPRLAND_INSTANCE_SIGNATURE"),
-        ".socket.sock",
-    )
+def hyprctl(cmd: str, buf_size: int = 2048):
     try:
-        s.connect(str(socket_path))
-        s.send(cmd.encode("utf-8"))
-
-        output = b""
-        while True:
-            buffer = s.recv(buf_size)
-            if buffer:
-                output = b"".join([output, buffer])
-            else:
-                break
-        s.close()
-        return output.decode("utf-8")
+        runtime_dir = os.getenv("XDG_RUNTIME_DIR")
+        if not runtime_dir:
+            raise KeyError("Enironment Variable Not Found")
+        hypr_instance = os.getenv("HYPRLAND_INSTANCE_SIGNATURE")
+        if not hypr_instance:
+            raise KeyError("Enironment Variable Not Found")
+        socket_path = Path(
+            runtime_dir,
+            "hypr",
+            hypr_instance,
+            ".socket.sock",
+        )
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(str(socket_path))
+            _ = s.send(cmd.encode("utf-8"))
+            output = b""
+            while True:
+                buffer = s.recv(buf_size)
+                if buffer:
+                    output = b"".join([output, buffer])
+                else:
+                    break
+            return output.decode("utf-8")
     except Exception as e:
         eprint(f"hyprctl: {e}")
         return ""
 
 
-def list_keyboards():
+def get_current_layout():
     o = hyprctl("j/devices")
     devices = json.loads(o)
-    return devices["keyboards"] if "keyboards" in devices else []
-
-
-def get_current_layout(keyboards):
+    keyboards = devices["keyboards"] if "keyboards" in devices else []
     for k in keyboards:
         if "keyboard" in k["name"]:
             return k["active_keymap"]
     return keyboards[0]["layout"]
 
 
-def refresh(keyboards):
-    label = get_current_layout(keyboards)
+def refresh():
+    label = get_current_layout()
     print(label)
 
 
-def change_layouts(keyboards):
-    for k in keyboards:
-        hyprctl(f"switchxkblayout {k['name']} next")
+def change_layouts():
+    hyprctl("switchxkblayout all next")
 
 
-def to_english(keyboards):
-    for k in keyboards:
-        hyprctl(f"switchxkblayout {k['name']} 0")
+def to_english():
+    hyprctl("switchxkblayout all 0")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    keyboards = list_keyboards()
     parser.add_argument(
         "-s", "--switch", action="store_true", help="switches keyboard layout"
     )
@@ -80,22 +78,22 @@ def main():
     )
     args = parser.parse_args()
     if args.switch:
+        change_layouts()
         subprocess.Popen(
             ["pkill", "-f", "-34", "nwg-panel"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
         )
-        change_layouts(keyboards)
         sys.exit(0)
     elif args.english:
+        to_english()
         subprocess.Popen(
             ["pkill", "-f", "-34", "nwg-panel"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
         )
-        to_english(keyboards)
         sys.exit(0)
-    refresh(keyboards)
+    refresh()
 
 
 if __name__ == "__main__":
