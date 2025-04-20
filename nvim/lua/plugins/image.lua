485de49e97ae3dc9e375f762e240fa97db4d2c63
Richard Garber <rg.1029384756@gmail.com>
2025-04-20 16:01:57 -0400

    Conditional image.lua on being on Linux

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -1,4 +1,5 @@
-if vim.fn.has("win32") == 1 then
+-- The assumption is: If we're on windows, we're not installing kitty. If we're on linux, we're probably installing kitty, though only conditionally activating
+if vim.fn.has "win32" == 1 then
     return {}
 end
 return {
@@ -16,28 +17,33 @@ return {
             end,
         },
     },
-    opts = {
-        backend = "kitty",
-        integrations = {
-            markdown = {
-                enabled = true,
-                clear_in_insert_mode = false,
-                download_remote_images = true,
-                only_render_image_at_cursor = false,
-                filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+    config = function()
+        local opts = {
+            backend = "kitty",
+            integrations = {
+                markdown = {
+                    enabled = true,
+                    clear_in_insert_mode = false,
+                    download_remote_images = true,
+                    only_render_image_at_cursor = false,
+                    filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+                },
+                neorg = {
+                    enabled = true,
+                    clear_in_insert_mode = false,
+                    download_remote_images = true,
+                    only_render_image_at_cursor = false,
+                    filetypes = { "norg" },
+                },
             },
-            neorg = {
-                enabled = true,
-                clear_in_insert_mode = false,
-                download_remote_images = true,
-                only_render_image_at_cursor = false,
-                filetypes = { "norg" },
-            },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        kitty_method = "normal",
-    },
+            max_width = nil,
+            max_height = nil,
+            max_width_window_percentage = nil,
+            max_height_window_percentage = 50,
+            kitty_method = "normal",
+        }
+        if os.getenv "KITTY_WINDOW_ID" then
+            require("image").setup(opts)
+        end
+    end,
 }
